71176b27f4c36d5b5d4817e1f0738808deee432c
Macdonald Nkhata <official.nkhata@gmail.com>
2023-02-22 14:24:39 +0200

    started styling the product section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    extend: {},
+    colors: {
+      'orange' : '#ff7d1a',
+      'pale-orange' : '#ffede0',
+      'very-dark-blue' : '#1d2025',
+      'dark-grayish-blue' : '#68707d',
+      'grayish-blue' : '#b6bcc8',
+      'light-grayish-blue' : '#f7f8fd',
+      'white' : '#ffffff',
+      'black' : '#000000'
+    },
+    extend: {
+      fontFamily: {
+        'Kumbh': ["Kumbh Sans"]
+      }
+    },
   },
   plugins: [],
 }
